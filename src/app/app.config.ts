02c711893cfdee7bcf717aa2196c3b8fe6b95519
Madhu sudhan Reddy <tippanamadhusudan@gmail.com>
2024-11-08 12:09:37 -0500

    congig file update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,13 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5avA9bM5cDgGS67VqU_kbicYz2CykYkQ",
-  authDomain: "shoppingapp-9c191.firebaseapp.com",
-  projectId: "shoppingapp-9c191",
-  storageBucket: "shoppingapp-9c191.firebasestorage.app",
-  messagingSenderId: "198236883651",
-  appId: "1:198236883651:web:43985dd5e94b1d4b23f076",
-  measurementId: "G-DVXJB1C163"
+  apiKey: "************",
+  authDomain: "******************",
+  projectId: "*****************",
+  storageBucket: "*****************",
+  messagingSenderId: "****************",
+  appId: "************************",
+  measurementId: "*********************"
 };
 
 export const appConfig: ApplicationConfig = {
